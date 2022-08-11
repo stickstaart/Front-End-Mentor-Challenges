23e148cead4a103eef7d5313c800313ef0d28772
[Joz] <[jzvndk@gmail.com]>
2022-08-11 12:04:17 +0200

    finished JavaScript

--- Interactive Rating Component/script.js
+++ Interactive Rating Component/script.js
@@ -1,5 +1,10 @@
 const radios = Array.from(document.querySelectorAll(`.radio`));
 const submitBtn = document.querySelector(".submit-btn");
+const ratingContainer = document.querySelector(".irc-container-rating");
+const thankyouContainer = document.querySelector(".irc-container-thankyou");
+const selection = document.querySelector(".selection");
+
+let selectedValue;
 
 radios.map((radio) => {
   radio.addEventListener(`click`, (e) => {
@@ -18,6 +23,11 @@ submitBtn.addEventListener("click", (e) => {
   radios.map((element) => {
     if (element.classList.contains("selected-radio")) {
       console.log(`You have selected ${element.id}!`);
+      selectedValue = element.id[element.id.length - 1].toString();
+      console.log(selectedValue);
+      ratingContainer.style.display = "none";
+      thankyouContainer.style.display = "flex";
+      selection.innerHTML = selectedValue;
     }
   });
 });
