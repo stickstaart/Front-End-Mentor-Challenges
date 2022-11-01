eb222cd18f4ab279657ff529d18e491ce8e82c54
[Joz] <[jzvndk@gmail.com]>
2022-11-01 10:41:57 +0100

    making DOM display work with JavaScript

--- rest-countries-api-with-color-theme-switcher-master/script.js
+++ rest-countries-api-with-color-theme-switcher-master/script.js
@@ -5,6 +5,8 @@ const countrydetailContainer = document.querySelector(
 const backBtn = document.querySelector(".back-btn");
 
 backBtn.addEventListener("click", () => {
+  const countryDetail = document.querySelector(".country-detail");
+  countryDetail.parentNode.removeChild(countryDetail);
   countrycardContainer.classList.remove("invisible");
   countrydetailContainer.classList.add("invisible");
 });
@@ -15,12 +17,13 @@ const apiUrl = "https://restcountries.com/v3.1/all";
 async function getCountries() {
   const response = await fetch(apiUrl);
   const data = await response.json();
+  console.log(data[140]);
   return data;
 }
 
 // INJECTING CODE INTO HTML //
 function displayCountries(allCountries) {
-  let id = 1;
+  let id = 0;
 
   allCountries.forEach((country) => {
     let div = document.createElement("div");
@@ -47,6 +50,56 @@ function displayCountries(allCountries) {
       countrycardContainer.classList.add("invisible");
       countrydetailContainer.classList.remove("invisible");
       console.log(e.target.id);
+      console.log(country.name.common);
+      const languages = Object.values(country.languages);
+      const currencies = Object.values(country.currencies);
+
+      let div = document.createElement("div");
+      div.classList.add("country-detail");
+      div.id = e.target.id;
+      div.insertAdjacentHTML(
+        "beforeend",
+        `
+        <img src=${country.flags.png} alt="flag-detail" class="flag-detail"/>
+
+        <div class="text-detail-top">
+          <h1 class="name">${country.name.common}</h1>
+          <h2 class="native-name">Native Name: <span class="h2text">${
+            country.name.official
+          }</span></h2>
+          <h2 class="population">Population: <span class="h2text">${country.population.toLocaleString()}</span></h2>
+          <h2 class="region">Region: <span class="h2text">${
+            country.region
+          }</span></h2>
+          <h2 class="sub-region">Sub Region: <span class="h2text" id="">${
+            country.subregion
+          }</span></h2>
+          <h2 class="capital" id="">Capital: <span class="h2text" id="">${
+            country.capital
+          }</span></h2>
+        </div>
+
+        <div class="text-detail-bottom">
+          <h2 class="top-level-domain">Top Level Domain: <span class="h2text">${
+            country.tld
+          }</span></h2>
+          <h2 class="currencies" id="">Currencies: <span class="h2text">${
+            currencies[0].name
+          }</span></h2>
+          <h2 class="languages" id="">Languages: <span class="h2text">${languages.join(
+            ", "
+          )}</span></h2>
+        </div>
+
+        <div class="border-countries">
+          <h2 class="border-countries-title" id="">Border Countries:</h2>
+          <button class="border-country" id="">Country1</button>
+          <button class="border-country" id="">Country2</button>
+          <button class="border-country" id="">Country3</button>
+        </div>
+      `
+      );
+      countrydetailContainer.appendChild(div);
     });
     id++;
   });
